b5a8705d13de70a00dc787d4b539d9fef53b52fe
JacobABrennan <jacobabrennan@gmail.com>
2019-12-03 23:37:16 -0500

    Migrate necessary fonts and css into javascript

--- logic/display.js
+++ logic/display.js
@@ -8,6 +8,7 @@
 
 //-- Dependencies --------------------------------
 import {
+    DOM_STYLE_DYNAMIC,
     DISPLAY_FONT,
     DISPLAY_WIDTH_DEFAULT,
     DISPLAY_HEIGHT_DEFAULT,
@@ -17,16 +18,37 @@ import {
 let context;
 
 //-- Setup (boot) --------------------------------
-export function setup(container) {
-    // Canvas
-    const canvasMain = document.createElement('canvas');
-    canvasMain.width  = DISPLAY_WIDTH_DEFAULT;
-    canvasMain.height = DISPLAY_HEIGHT_DEFAULT;
+export function setup(elementContainer) {
+    // Create display canvas
+    const elementCanvas = document.createElement('canvas');
+    elementCanvas.width  = DISPLAY_WIDTH_DEFAULT;
+    elementCanvas.height = DISPLAY_HEIGHT_DEFAULT;
+    // Add font style sheet
+    const elementStyle = document.createElement('style');
+    elementStyle.innerText = DOM_STYLE_DYNAMIC;
+    document.head.appendChild(elementStyle);
+    // Configure container/canvas CSS
+    elementContainer.style.outline = 'none';
+    elementContainer.style.overflow = 'hidden';
+    elementCanvas.style.background = 'black';
+    elementCanvas.style.width = '100%';
+    elementCanvas.style.height = '100%';
+    elementCanvas.style.objectFit = 'contain';
+    elementCanvas.style.imageRendering = 'crisp-edges';
     // Container
-    container.tabIndex = 1;
-    container.appendChild(canvasMain);
+    elementContainer.tabIndex = 1;
+    elementContainer.appendChild(elementCanvas);
     // Context
-    context = canvasMain.getContext('2d');
+    context = elementCanvas.getContext('2d');
     context.imageSmoothingEnabled = false;
     context.font = DISPLAY_FONT;
+    // Test
+    context.fillStyle = 'black';
+    context.fillRect(0,0,DISPLAY_WIDTH_DEFAULT,DISPLAY_HEIGHT_DEFAULT);
+    context.fillStyle = 'white';
+    setInterval(
+        function () { context.fillText('Loaded', 0, 32);},
+        100,
+    );
+    context.fillText(DISPLAY_FONT, 0, 16);
 }
